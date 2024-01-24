9a11035386aa931748759b296edf3180cbe2239f
ztmzzz <ztmzzz@163.com>
2024-01-24 18:28:12 +0800

    增加系统时间，优化切换

--- ESP32_TF_Control.ino
+++ ESP32_TF_Control.ino
@@ -11,10 +11,13 @@
 #include <ESPmDNS.h>
 #include "web.h"  // web、样式文件
 #include <SD_MMC.h>
+#include "time.h"
 
 // WIFI的SSID和PASSWORD,可以在下边代码中改创建热点或连接WIFI
 char ssid[] = "ssid";
 char pass[] = "password";
+
+const char *ntpServer = "pool.ntp.org";
 #define servername "tfserver"  // 定义服务器的名字
 #define CTRLTF 15              // 1=TF连接ESP32
 #define CTRL_483 16            // 0=切换芯片开
@@ -23,6 +26,7 @@ char pass[] = "password";
 
 AsyncWebServer server(80);
 bool SD_present = false;  // 控制TF卡是否存在
+bool handle = false;
 
 void setup(void) {
   pinMode(11, INPUT_PULLUP);
@@ -45,8 +49,8 @@ void setup(void) {
   /* 
   WiFi.softAP(ssid, pass); // 生成WIFI热点
   */
-  /////////////////////////////////////////////////////
-  WiFi.begin(ssid, pass);  // 连接WIFI
+  //连接WIFI
+  WiFi.begin(ssid, pass);
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -55,6 +59,18 @@ void setup(void) {
   Serial.println("");
   Serial.print("Connected,IP adrress:");
   Serial.println(WiFi.localIP());
+  //设置系统时间
+  configTime(3600 * 8, 0, ntpServer);
+  while (time(nullptr) < 24 * 3600) {
+    Serial.print(".");
+    delay(1000);
+  }
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
   /*--------------------------------------------------*/
   // 设置服务器名称，如果上边定义服务器的名字为tfserver，则可以使用 http://tfserver.local/ 进入。
   if (!MDNS.begin(servername)) {
@@ -143,17 +159,22 @@ void deleteFile(AsyncWebServerRequest *request) {
     request->send(500, "text/plain", "删除失败");
   }
 }
-// 将TF卡连接到ESP32
+//将TF卡连接到ESP32
 void connect(AsyncWebServerRequest *request) {
+  if (handle) {
+    request->send(503, "text/plain", "正在切换");
+    return;
+  }
+  handle = true;
   SD_MMC.end();
   digitalWrite(CTRL_483, HIGH);   // 关闭切换芯片
   digitalWrite(TFPWR_EXT, HIGH);  // 关闭所有电源
   digitalWrite(TFPWR_32, HIGH);
-  delay(1000);
+  delay(500);
   digitalWrite(CTRL_483, LOW);  // 开启切换芯片
   digitalWrite(CTRLTF, HIGH);   // TF连接到ESP32
   digitalWrite(TFPWR_32, LOW);  // TF电源用ESP32
-  delay(2000);
+  delay(1000);
   if (!SD_MMC.begin("/sdcard", false, false, 80000, 10)) {
     Serial.println("初始化TF卡失败");
     SD_present = false;
@@ -161,24 +182,31 @@ void connect(AsyncWebServerRequest *request) {
     Serial.println("初始化TF卡成功");
     SD_present = true;
   }
+  handle = false;
   request->send(200, "text/plain", String(SD_present));
 }
-// 将TF卡连接到外部设备(3D打印机)
+//将TF卡连接到外部设备(3D打印机)
 void disconnect(AsyncWebServerRequest *request) {
+  if (handle) {
+    request->send(503, "text/plain", "正在切换");
+    return;
+  }
+  handle = true;
   SD_MMC.end();
   digitalWrite(CTRL_483, HIGH);   // 关闭切换芯片
   digitalWrite(TFPWR_EXT, HIGH);  // 关闭所有电源
   digitalWrite(TFPWR_32, HIGH);
-  delay(1000);
+  delay(500);
   digitalWrite(CTRL_483, LOW);   // 开启切换芯片
   digitalWrite(CTRLTF, LOW);     // TF连接到外部设备
   digitalWrite(TFPWR_EXT, LOW);  // TF电源用外部设备
-  delay(2000);
+  delay(1000);
   SD_present = false;
+  handle = false;
   request->send(200, "text/plain", "1");
 }
 unsigned long startTime, endTime;
-// 上传文件到TF卡
+//上传文件到TF卡
 File uploadFile;
 void handleFileUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
   if (!index) {
